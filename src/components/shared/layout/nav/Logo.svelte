a10f50f5d01b578e4d3a380b06532f469adc6afb
Alessandro Chumpitaz <par_214@hotmail.com>
2023-04-05 01:52:25 -0500

    :sparkles: Add nav

---
+++ src/components/shared/layout/nav/Logo.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { IcosahedronGeometry, MeshStandardMaterial } from 'three';
+	import { DirectionalLight, Mesh, useFrame } from '@threlte/core';
+	import { goto } from '$app/navigation';
+
+	let rotation: number = 0;
+	useFrame(() => {
+		rotation += 0.004;
+	});
+
+	$: material = new MeshStandardMaterial({ color: '#38bdf8' });
+</script>
+
+<DirectionalLight position={{ y: 15, z: 10 }} />
+<Mesh
+	interactive
+	rotation={{
+		x: rotation,
+		y: rotation,
+		z: rotation
+	}}
+	on:pointerenter={() => {
+		material.color.set('#6366f1');
+	}}
+	on:pointerleave={() => {
+		material.color.set('#38bdf8');
+	}}
+	on:click={() => {
+		material.color.set('#f59e0b');
+		goto('/')
+	}}
+	geometry={new IcosahedronGeometry(3.3)}
+	{material}
+/>
---
+++ src/components/shared/layout/nav/Nav.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Canvas } from '@threlte/core';
+	import Logo from './Logo.svelte';
+	import Bars3Icon from '../../icons/Bars3Icon.svelte';
+	import XMarkIcon from '../../icons/XMarkIcon.svelte';
+
+	const buttons = [
+		{ to: '/experience', label: 'Experience' },
+		{ to: '/projects', label: 'Projects' },
+		{ to: '/about', label: 'About Me' }
+	];
+
+	// Reactive variables
+	$: open = false;
+
+	$: isActive = (path: string): boolean => {
+		return path === $page.url.pathname;
+	};
+</script>
+
+<nav class="bg-gray-800">
+	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+		<div class="relative flex h-16 items-center justify-between">
+			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+				<!-- Mobile menu button-->
+				<button
+					type="button"
+					class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+					aria-controls="mobile-menu"
+					aria-expanded="false"
+					on:click={() => {
+						open = !open;
+					}}
+				>
+					<span class="sr-only">Open main menu</span>
+					{#if !open}
+						<Bars3Icon />
+					{:else}
+						<XMarkIcon />
+					{/if}
+				</button>
+			</div>
+			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+				<div class="flex flex-shrink-0 items-center">
+					<div class="block h-8 lg:hidden cursor-pointer">
+						<Canvas size={{ width: 30, height: 30 }}>
+							<Logo />
+						</Canvas>
+					</div>
+				</div>
+				<div class="hidden sm:ml-6 sm:block">
+					<div class="flex space-x-4 items-center">
+						{#each buttons as button}
+							<a
+								href={button.to}
+								class={isActive(button.to)
+									? 'bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+									: 'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'}
+								aria-current="page">{button.label}</a
+							>
+						{/each}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Mobile menu, show/hide based on menu state. -->
+	{#if open}
+		<div class="sm:hidden" id="mobile-menu">
+			<div class="space-y-1 px-2 pb-3 pt-2">
+				<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+				{#each buttons as button}
+					<a
+						href={button.to}
+						class={isActive(button.to)
+							? 'bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
+							: 'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'}
+						aria-current="page">{button.label}</a
+					>
+				{/each}
+			</div>
+		</div>
+	{/if}
+</nav>
