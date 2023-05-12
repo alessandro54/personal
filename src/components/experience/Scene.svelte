1afdea18b3e872dc283241d591926ad5ea8d61f8
Alessandro Chumpitaz <par_214@hotmail.com>
2023-05-11 23:48:34 -0500

    :sparkles: add 3d arrow

--- src/components/experience/Scene.svelte
+++ src/components/experience/Scene.svelte
@@ -1,52 +1,18 @@
 <script lang="ts">
-	import { T, useFrame } from '@threlte/core';
-	import { HTML, OrbitControls, interactivity} from '@threlte/extras';
-	import { Color, MeshStandardMaterial, IcosahedronGeometry } from 'three';
-	import { spring } from 'svelte/motion';
+  import { T } from '@threlte/core'
+  import { useGltf } from '@threlte/extras'
+  import { MeshBasicMaterial } from 'three'
 
-	const getRandomColor = () =>
-		`#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	const gltf = useGltf('/models/arrow.glb')
+	console.log(gltf)
 
-	let material = new MeshStandardMaterial({
-		color: new Color(getRandomColor())
-	});
-
-  useFrame(() => {
-		rotation += 0.004;
-	});
-
-  let rotation: number = 0;
-
-	const onClick = () => {
-		material.color.set(getRandomColor());
-		material = material;
-	};
-
-  $: material = new MeshStandardMaterial({ color: '#38bdf8' });
-  interactivity();
-
-	let isHovering = false;
-	let isPointerDown = false;
-
-	let htmlPosZ = spring(0);
 </script>
 
-<T.DirectionalLight position.y={15} position.z={10} />
-<T.Mesh
-	interactive
-  rotation.x={rotation}
-	rotation.y={rotation}
-	rotation.z={rotation}
-	on:pointerenter={() => {
-		material.color.set('#6366f1');
-	}}
-	on:pointerleave={() => {
-		material.color.set('#38bdf8');
-	}}
-	on:click={() => {
-		material.color.set('#f59e0b');
-	}}
-	geometry={new IcosahedronGeometry(3.3)}
-	{material}
-/>
+{#if $gltf}
+	<T.PerspectiveCamera position={[1,10,6]}/>
+	<T.Mesh
+		geometry={$gltf.scene.children[0].geometry}
+		material={new MeshBasicMaterial({ color: '#38bdf8' })}
+	/>
+{/if}
 
